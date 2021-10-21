44a20dfdbd73490d8723979ee2a085581309b0ed
AveCool <79378189+ave-cool@users.noreply.github.com>
2021-10-21 19:45:02 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const Discord = require('discord.js');
+const { Client, MessageEmbed } = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+ client.user.setActivity('ping', { type: 'STREAMING' })
+})
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+client.on('message', message => {
+  if (message.content === 'ping') {
+    const embed = new MessageEmbed()
+      .setTitle('pong')
+      .setColor(`RANDOM`)
+      .setDescription(`🏓${client.ws.ping}ms `);
+    message.channel.send(embed);
   }
 });
 
-client.login('ODk0OTc2MjgxODM2NDIxMTcx.YVx1nQ.oNE33k_BrIfi5XwH36kSMxxYMBg');
+client.on('message', message => {
+  if (message.content === 'hi') {
+    const embed = new MessageEmbed()
+      .setTitle('Hello')
+      .setColor(`RANDOM`)
+      .setDescription('```how are u my friend :)```');
+    message.channel.send(embed);
+  }
+});
+
+
+client.login(process.env.token);//if ur using replit then go to that lock🔒and click on it then make a file name it token then put ur token under that :)
